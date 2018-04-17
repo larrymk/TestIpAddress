d681e0e49defc0d5087650b46182ac16374859e1
Larry Kereh <larry.kereh@geekseat.com.au>
2018-04-17 16:59:23 +0800

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestIpAddress.Models;
+
+namespace TestIpAddress.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public HomeController(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public IActionResult Index()
+        {
+            var ipaddress = _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            return Json(ipaddress);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
